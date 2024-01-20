b72ee88ddc2fd0e72c13af16faa11f950028013a
Yan Osher Cohen <74152828+yanoshercohen@users.noreply.github.com>
2024-01-20 23:44:23 +0200

    Update one_time_everything.js

--- scripts/one_time_everything.js
+++ scripts/one_time_everything.js
@@ -1,27 +1,19 @@
 const db = indexedDB.open('model-storage');
 
 db.onsuccess = (event) => {
-    const model_storage = event.target.result;
-    const messages = model_storage.transaction(['message'], 'readwrite').objectStore('message');
-    const keys_request = messages.getAllKeys();
-    keys_request.onsuccess = (event) => {
-        const keys = event.target.result;
-        for (const key of keys) {
-            const request = messages.get(key);
-            request.onsuccess = (event) => {
-                const data = event.target.result;
-                if (data?.isViewOnce !== true) {
-                    return;
-                }
-                data.isViewOnce = false;
-                const requestUpdate = messages.put(data);
-                requestUpdate.onerror = (event) => {
-                    console.log('ERROR')
-                };
-                requestUpdate.onsuccess = (event) => {
-                    console.log('SUCCESS')
-                };
-            }
-        }
-    }
+	const model_storage = event.target.result;
+	const messages = model_storage.transaction(['message'], 'readwrite').objectStore('message');
+	messages.getAllKeys().onsuccess = (event) => {
+		event.target.result.forEach((key) => {
+			const request = messages.get(key);
+			request.onsuccess = (event) => {
+				const data = event.target.result;
+				if (data?.isViewOnce) {
+					data.isViewOnce = false;
+					messages.put(data).onsuccess = () => console.log('SUCCESS');
+					messages.put(data).onerror = () => console.log('ERROR');
+				}
+			}
+		});
+	}
 }
