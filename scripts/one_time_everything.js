f5cb51f9e3cd296bfb6a532bde8bbc4a5a233fc9
Yan Osher Cohen <74152828+yanoshercohen@users.noreply.github.com>
2024-01-20 23:53:42 +0200

    Update one_time_everything.js

--- scripts/one_time_everything.js
+++ scripts/one_time_everything.js
@@ -1,19 +1,18 @@
 const db = indexedDB.open('model-storage');
 
 db.onsuccess = (event) => {
-	const model_storage = event.target.result;
-	const messages = model_storage.transaction(['message'], 'readwrite').objectStore('message');
-	messages.getAllKeys().onsuccess = (event) => {
-		event.target.result.forEach((key) => {
-			const request = messages.get(key);
-			request.onsuccess = (event) => {
-				const data = event.target.result;
-				if (data?.isViewOnce) {
-					data.isViewOnce = false;
-					messages.put(data).onsuccess = () => console.log('SUCCESS');
-					messages.put(data).onerror = () => console.log('ERROR');
-				}
-			}
-		});
-	}
+    const model_storage = event.target.result;
+    const messages = model_storage.transaction(['message'], 'readwrite').objectStore('message');
+    messages.getAllKeys().onsuccess = (event) => {
+        event.target.result.forEach((key) => {
+            const request = messages.get(key);
+            request.onsuccess = (event) => {
+                const data = event.target.result;
+                if (data?.isViewOnce) {
+                    data.isViewOnce = false;
+                    messages.put(data).onsuccess = () => console.log('SUCCESS');
+                }
+            }
+        });
+    }
 }
